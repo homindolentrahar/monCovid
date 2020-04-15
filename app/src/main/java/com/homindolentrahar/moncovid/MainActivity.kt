786b44e089523cef6147e381ebfcbe7ce76a3e56
homindolentrahar <rahardiyanekowidiatmoko@gmail.com>
2020-04-15 22:21:33 +0700

    Add Dependencies

---
+++ app/src/main/java/com/homindolentrahar/moncovid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homindolentrahar.moncovid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
